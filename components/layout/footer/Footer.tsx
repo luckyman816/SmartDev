cc8bc854d7cc34c77f2f3fbc19d4c0ee60144d20
Michael Moore <zhangyunpei816@gmail.com>
2024-03-14 01:53:06 +1000

    update

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -69,7 +69,7 @@ const Footer = () => {
                     <p>
                       Copyright &copy;{" "}
                       <span id="copyrightYear">{currentYear}</span>{" "}
-                      <Link href="/">John Chia</Link>. All Rights Reserved
+                      <Link href="/">Steven Lee</Link>. All Rights Reserved
                     </p>
                   </div>
                 </div>
