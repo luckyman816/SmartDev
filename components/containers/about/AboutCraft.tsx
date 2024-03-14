365c641ef4d603c9e37084c4e847903547026dce
Michael Moore <zhangyunpei816@gmail.com>
2024-03-15 00:54:16 +1000

    update

--- components/containers/about/AboutCraft.tsx
+++ components/containers/about/AboutCraft.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import CraftThumb from "@/public/images/craft-thumb.png";
 import Shape from "@/public/images/footer/shape-one.png";
 
 const AboutCraft = () => {
@@ -28,9 +27,7 @@ const AboutCraft = () => {
           </div>
           <div className="col-12 col-lg-6 col-xxl-7">
             <div className="craft__thumb text-start text-lg-end">
-              <div className="reveal-img parallax-img">
-                <Image src={CraftThumb} alt="Image" priority />
-              </div>
+              
             </div>
           </div>
         </div>
