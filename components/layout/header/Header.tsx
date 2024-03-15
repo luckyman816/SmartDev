70a0469560ba0a2f7451b45b4a7f70b01275cfd4
Michael Moore <zhangyunpei816@gmail.com>
2024-03-16 04:49:18 +1000

    update

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -79,7 +79,7 @@ const Header = () => {
                 <nav className="navbar p-0">
                   <div className="navbar__menu">
                     <ul className="navbar__list">
-                    &#10029;Welcome to Steven Lee&#10076;s personal website.&#10029;
+                    &#10029;Welcome to Takao Miyazaki&#10076;s personal website.&#10029;
                       {NavbarData.map((item, index) => {
                         return <MenuItem key={index} item={item} />;
                       })}
